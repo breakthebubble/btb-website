26a0a41b6fba6606e8047ac01af6a3ed0a3a424f
svdarren <3759672+svdarren@users.noreply.github.com>
2020-01-02 12:41:46 -0600

    debug msg

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -13,6 +13,7 @@ fi
 curl -s -X POST https://api.stackbit.com/project/5e0aa13cd14934001b371696/webhook/build/ssgbuild > /dev/null
 
 ./eventbrite.sh './_data/eventbrite.json'
+cat './_data/eventbrite.json'
 jekyll build
 
 curl -s -X POST https://api.stackbit.com/project/5e0aa13cd14934001b371696/webhook/build/publish > /dev/null
