6f3c5a1506cbabf2735bc55a399754dbf3254a8d
svdarren <3759672+svdarren@users.noreply.github.com>
2020-01-04 18:02:16 -0600

    added httparty based script

---
+++ eventbrite.rb
@@ -0,0 +1,42 @@
+require 'json'
+require 'httparty'
+
+if ARGV.length != 1
+    puts "Wrong number of arguments"
+    puts "Usage: 'ruby eventbrite.rb /path/to/output/json'"
+    exit
+end
+
+options = { query: { token: ENV["EVENTBRITE_PRIVATE_TOKEN"] } }
+
+h_user = HTTParty.get("https://www.eventbriteapi.com/v3/users/me/", options).parsed_response
+puts "User ID: " + h_user_id = h_user['id']
+
+h_orgs = HTTParty.get("https://www.eventbriteapi.com/v3/users/me/organizations/", options).parsed_response
+puts "Organization ID: " + h_org_id = h_orgs['organizations'][0]['id']
+
+events_options = options
+#events_options = { query: { token: ENV["EVENTBRITE_PRIVATE_TOKEN"], status: 'live' } }
+h_event_resp = HTTParty.get("https://www.eventbriteapi.com/v3/organizations/"+h_org_id+"/events/", events_options).parsed_response
+h_events = h_event_resp['events']
+while h_event_resp['pagination']['has_more_items']
+    events_options = { query: { token: ENV["EVENTBRITE_PRIVATE_TOKEN"], continuation: h_event_resp['pagination']['continuation'] } }
+    h_event_resp = HTTParty.get("https://www.eventbriteapi.com/v3/organizations/"+h_org_id+"/events/", events_options).parsed_response
+    h_events = h_events + h_event_resp['events']
+end
+
+puts "Number of Events in Organization: " + h_events.length.to_s()
+h_events.each do |n|
+    if n['organization_id'] != h_org_id
+        puts "Found event with different Organization"
+        puts "organization_id = " + n['organization_id']
+    end
+    if n['organizer_id'] != "4178289715"
+        puts "Found event within Organization with different Organizer"
+        puts "organizer_id = " + n['organizer_id']
+    end
+end
+
+File.open(ARGV[0],"w") do |f|
+    f.write(h_events.to_json)
+end
